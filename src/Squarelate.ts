c17c0f51f671d9b5b632eef1ba4fd4f5144d6f17
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2020-11-02 02:46:46 +0100

    js -> ts

--- src/Squarelate.ts
+++ src/Squarelate.ts
@@ -1,73 +1,73 @@
 export namespace Squarelate {
-  export function init(githubUrl: string) {
-    function translatedImage(url, list, lang) {
-      const fileArgs = url.substring(url.lastIndexOf('/') + 1);
-      const qIndex = fileArgs.indexOf('?');
-      let file = fileArgs;
+  export const init = (githubUrl: string) => {
+    const translatedImage = (url: string, list: string[], lang: string): string => {
+      const fileArgs = url.substring(url.lastIndexOf('/') + 1)
+      const qIndex = fileArgs.indexOf('?')
+      let file = fileArgs
       if (qIndex >= 0) {
-        file = fileArgs.substring(0, qIndex);
+        file = fileArgs.substring(0, qIndex)
       }
       if (list.includes(file)) {
-        return `${githubUrl}/${lang}/images/${fileArgs}`;
+        return `${githubUrl}/${lang}/images/${fileArgs}`
       }
-      return url;
+      return url
     }
-    function translatedPdf(url, list, lang) {
+    const translatedPdf = (url: string, list: string[], lang: string): string => {
       if (url.startsWith('/s/')) {
-        const file = url.substring(3);
+        const file = url.substring(3)
         if (list.includes(file)) {
-          return `${githubUrl}/${lang}/assets/${file}`;
+          return `${githubUrl}/${lang}/assets/${file}`
         }
       }
-      return url;
+      return url
     }
-    $(function() {
+    $(() => {
       const queryDict = {}
-      location.search.substr(1).split("&").forEach(function(item) {
+      location.search.substr(1).split("&").forEach((item) => {
         queryDict[item.split("=")[0]] = item.split("=")[1]
       })
-      let lang = 'en';
-      const savedLang = localStorage.getItem('ecv-lang');
+      let lang = 'en'
+      const savedLang = localStorage.getItem('ecv-lang')
       if (queryDict.hasOwnProperty('lang')) {
-        lang = queryDict['lang'];
+        lang = queryDict['lang']
       } else if (savedLang) {
         lang = savedLang
       }
-      localStorage.setItem('ecv-lang', lang);
-      history.pushState({}, '', `${window.location.pathname}?lang=${lang}`);
+      localStorage.setItem('ecv-lang', lang)
+      history.pushState({}, '', `${window.location.pathname}?lang=${lang}`)
       if (lang !== 'en') {
-        $.getJSON(`${githubUrl}/${lang}/dict.json`, function(data) {
+        $.getJSON(`${githubUrl}/${lang}/dict.json`, (data) => {
           $('p, span, :header, a').contents()
             .each(function() {
               if (this.nodeType == 3 && data.hasOwnProperty(this['data'])) {
-                this['data'] = data[this['data']];
+                this['data'] = data[this['data']]
               }
-            });
-        });
-        $.getJSON(`${githubUrl}/${lang}/assets/list.json`, function(data) {
+            })
+        })
+        $.getJSON(`${githubUrl}/${lang}/assets/list.json`, (data) => {
           $('a').each(function() {
-            $(this).attr('href', function(i, href) {
+            $(this).attr('href', (i, href) => {
               if (href) {
-                return translatedPdf(href, data, lang);
+                return translatedPdf(href, data, lang)
               }
-            });
-          });
-        });
-        $.getJSON(`${githubUrl}/${lang}/images/list.json`, function(data) {
+            })
+          })
+        })
+        $.getJSON(`${githubUrl}/${lang}/images/list.json`, (data) => {
           $('img').each(function() {
-            $(this).attr('data-src', function(i, src) {
+            $(this).attr('data-src', (i, src) => {
               if (src) {
-                return translatedImage(src, data, lang);
+                return translatedImage(src, data, lang)
               }
-            });
-            $(this).attr('src', function(i, src) {
+            })
+            $(this).attr('src', (i, src) => {
               if (src) {
-                return translatedImage(src, data, lang);
+                return translatedImage(src, data, lang)
               }
-            });
-          });
-        });
+            })
+          })
+        })
       }
-    });
+    })
   }
 }
